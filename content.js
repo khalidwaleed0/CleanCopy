ff556cb49aff436a8d925ecbcfa9b052f90bb510
Khalid Waleed <khalidwaleed0@outlook.com>
2022-07-03 03:09:23 +0200

    Initial commit

---
+++ content.js
@@ -0,0 +1,51 @@
+//await new Promise(r => setTimeout(r, 2000));
+const copyButton = document.createElement('div');
+copyButton.setAttribute("class","copyButton");
+copyButton.textContent = "Copy Link";
+//copyButton.style = "font-size: 15px; font-weight: 500; align-self: center; color: #65676B; margin-right: 8px; padding: 8px;";
+//copyButton.addEventListener("mouseover",()=>{copyButton.style = copyButton.getAttribute("style") + " background-color: #F2F2F2;"});
+//copyButton.addEventListener("mouseout",()=>{copyButton.style = copyButton.getAttribute("style") + " background-color: #fff;"});
+copyButton.addEventListener("mouseover",(e,s)=>{e.setAttribute("clicked","yes")});
+
+setInterval(function () {
+  let j = 0;
+  let links = [];
+  let posts = document.querySelectorAll("div[aria-labelledby][role='article']");
+  let length = posts.length;
+  for(i=0 ; i<length ; i++)
+  {
+    if(checkVisible(posts[i]) && parent.querySelector("div.copyButton") === null)
+    {
+      let url = posts[i].querySelectorAll("a[aria-label]")[2].getAttribute("href");
+      let cleanUrl = getCleanUrl2(url);
+      links.push(posts[i].querySelectorAll("a[aria-label]")[2].getAttribute("href"));
+      let parent = posts[i].querySelector("div[aria-label='Like']").parentElement.parentElement;
+      let node = copyButton.cloneNode(true);
+      node.setAttribute("href",cleanUrl)
+      parent.append(node);
+        
+      j++;
+    }
+  }
+    //chrome.runtime.sendMessage({todo: "showLength", count: posts.length, visiblePosts: j, urls: links});
+  }, 1000);
+
+
+function checkVisible(elm)
+{
+  var rect = elm.getBoundingClientRect();
+  var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+  return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
+
+function getCleanUrl(/**@type String */ url)
+{
+  return url.replace("/&+","").replace("posts/");
+}
+function getCleanUrl2(/**@type String */ url)
+{
+  return url.replace(/&.+/,"").replace("?multi_permalinks=","posts/");
+}
+
+// ابقى استثني لما يفتح فيديو معين ميدورش في البوستات الجانبية اللي فيه
+// remove everything after '?'
---
+++ eventPage.js
@@ -0,0 +1,13 @@
+console.log(cleanUrl("https://www.facebook.com/groups/218151142047187/?multi_permalinks=1321456788383278&hoisted_section_header_type=recently_seen"));
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+  if(request.todo === "showLength")
+  {
+    console.log(request.count);
+    console.log("visible posts : " + request.visiblePosts);
+    console.log("urls : " + request.urls);
+  }
+});
+function cleanUrl(/**@type String */ url)
+{
+  return url.replace(/&.+/,"").replace("?multi_permalinks=","posts/");
+}
