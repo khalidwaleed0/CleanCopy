d45fb29f2ed6f3d00ad5b1bb6aeddd559af215ed
Khalid Waleed <khalidwaleed0@outlook.com>
2025-03-13 19:10:37 +0200

    Revived Project (Fixed css selectors)

--- content.js
+++ content.js
@@ -1,113 +1,72 @@
 let str_copyLink = "Copy Link";
 let str_copied = "Copied !";
-let str_sponsored = "Sponsored";
 let str_likeButtonSelector = "div[aria-label='Like'], div[aria-label*='Remove']";
 let lang = document.querySelector("html").getAttribute("lang");
-if(lang === "ar")
-{
-  str_sponsored = "مُموَّل";
+if (lang === "ar") {
   str_likeButtonSelector = "div[aria-label='أعجبني'], div[aria-label*='إزالة']";
   str_copyLink = "نسخ الرابط";
   str_copied = "تم النسخ";
 }
 
-const copyButton = document.createElement('div');
-copyButton.setAttribute("class","copyButton");
+const copyButton = document.createElement("div");
+copyButton.setAttribute("class", "copyButton");
 copyButton.textContent = str_copyLink;
 
-
 setInterval(function () {
-  /**@type NodeListOf<Element> */
-  let posts = document.querySelectorAll("div[aria-labelledby][role='article']");
-  let length = posts.length;
-  for(i=0 ; i<length ; i++)
-  {
-    if(checkVisible(posts[i]))
-    {
-      let parent = posts[i].querySelector(str_likeButtonSelector).parentElement.parentElement;
-      if(parent.querySelector("div.copyButton") === null)
-      {
-        let postUrl = getPostUrl(posts[i]);
-        let cleanUrl = getCleanUrl(postUrl);
-        addCopyButton(parent, cleanUrl);
-      }
+  const posts = document.querySelectorAll("div[role=main] div[class=x1lliihq]:has(div[aria-label='Like'])");
+  const length = posts.length;
+  for (i = 0; i < length; i++) {
+    if (checkVisible(posts[i])) {
+      const bottomBar = posts[i].querySelector(str_likeButtonSelector).parentElement.parentElement;
+      if (!bottomBar.querySelector("div.copyButton")) addCopyButton(bottomBar, posts[i]);
     }
   }
-  }, 400);
+}, 400);
 
-function checkVisible(elm)
-{
-  var rect = elm.getBoundingClientRect();
-  var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+function checkVisible(elm) {
+  const rect = elm.getBoundingClientRect();
+  const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
   return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
 }
 
-function addCopyButton(parent, cleanUrl)
-{
+function addCopyButton(bottomBar, postElement) {
   let node = copyButton.cloneNode(true);
-  node.setAttribute("href",cleanUrl);
-  parent.append(node);
-  node.addEventListener("click",(e,h)=>{
-    copy(node,cleanUrl);
-    e.target.className = "copyButton clickedCopyButton";
+  bottomBar.append(node);
+  node.addEventListener("click", async (e, h) => {
+    let url = await getPostUrl(postElement);
+    let cleanUrl = getCleanUrl(url);
+    e.target.focus();
+    navigator.clipboard.writeText(cleanUrl);
+    e.target.className = "copyButton clicked";
     e.target.textContent = str_copied;
-    setTimeout(()=>{
+    setTimeout(() => {
       e.target.textContent = str_copyLink;
       e.target.className = "copyButton";
-    }, 400);
+    }, 1000);
   });
 }
 
-function getPostUrl(post)
-{
-  let postUrlElement =  getNormalPostUrlElement(post)?? getEventUrlElement(post)?? getNearestUrlElement(post);
-  if(postUrlElement.getAttribute("href").includes("https://www.facebook.com/?__"))
-    postUrlElement = postUrlElements[postUrlElements.length-1];
-  else if(postUrlElement.getAttribute("aria-label") === str_sponsored)
-    postUrlElement = getSponsoredUrlElement(post);
-  return postUrlElement.getAttribute("href");
+async function getPostUrl(post) {
+  post.querySelectorAll("a[role=link]").forEach((urlElement) => urlElement.focus({ preventScroll: true }));
+  await new Promise((r) => setTimeout(r, 100));
+  let urlElements = post.querySelectorAll("a[role=link]");
+  for (let urlElement of urlElements) {
+    if (/(multi_permalinks)|(\/posts\/)/gm.test(urlElement.href)) return urlElement.href;
+  }
+  for (let urlElement of urlElements) {
+    if (/(\/watch\/)|(\/photo\/)|(permalink\.php)/gm.test(urlElement.href)) return urlElement.href;
+  }
+  return null;
 }
 
-function getSponsoredUrlElement(post)
-{
-  postUrlElements = post.querySelectorAll("a[aria-label]");
-  if(postUrlElements.length >= 2)
-    return postUrlElements[2];
-  postUrlElements = post.querySelectorAll("a");
-  return postUrlElements[postUrlElements.length-1];
-}
-
-function getNormalPostUrlElement(post)
-{
-  return post.querySelector("span[id] a[aria-label]");
-}
-
-function getEventUrlElement(post)
-{
-  return post.querySelector("div[aria-labelledby][role='article'] div[id] a");
-}
-
-function getNearestUrlElement(post)
-{
-  return post.querySelectorAll("span a[aria-label]")[1];
-}
-
-function getCleanUrl(/**@type String */ url)
-{
-  url = url.replace(/&[^(id)].+/,"").replace("?multi_permalinks=","posts/").replace(/\?__.+/,"");
-  if(url.startsWith("/"))
-    url = "https://www.facebook.com" + url;
-  return decodeURIComponent(url).replace("https://l.facebook.com/l.php?u=","").replace(/(&|\?)utm.+/,"").replace(/(&|\?)fbclid.+/,"");
-}
-
-function copy(node,url)
-{
-  let input = document.createElement('textarea');
-  input.style = "opacity: 0;"
-  node.append(input);
-  input.value = url;
-  input.select();
-  input.focus();
-  document.execCommand("copy");
-  input.remove();
+function getCleanUrl(/**@type String */ url) {
+  if (url) {
+    url = url
+      .replaceAll(/\?hoisted_section_header_type=recently_seen&/gm, "")
+      .replaceAll("multi_permalinks=", "posts/");
+    url = url.replaceAll(/(&|\?)__cft.+/gm, "").replaceAll(/&set=.+/gm, "");
+    url = url.replaceAll(/\?comment_id=.+/gm, "");
+    return decodeURIComponent(url);
+  }
+  return "";
 }
